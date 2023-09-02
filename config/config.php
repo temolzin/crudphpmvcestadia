a0e657a39bf44b854bae0a8b5d3597d6f78aa6f9
JaimePrieto-15 <jaime.prieto2001@gmail.com>
2023-09-01 19:01:05 -0600

    Se agregaron las nuevas modificaciones del crud de Peliculas 2

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
     define('URL', 'http://localhost/crudphpmvcestadia/');
     define('HOST', 'localhost');
-    define('DB', 'crudpeliculas');
+    define('DB', 'estructuramvc');
     define('USER', 'root');
-    define('PASSWORD', '12345');
+    define('PASSWORD', '');
     define('CHARSET', 'utf8mb4');
 ?>
