23cb78343fed8deda25a1f46d38a87669b429ca9
SJAmanda <157259584+SJAmanda@users.noreply.github.com>
2025-09-09 11:55:27 -0600

    Update config.php
    Edite es archivo para dejarlo en su forma original, elimine el try catch

--- config/config.php
+++ config/config.php
@@ -6,12 +6,5 @@ define('USER', 'root');
 define('PASSWORD', '');
 define('CHARSET', 'utf8mb4');
 
-try {
-    $dsn = "mysql:host=" . HOST . ";dbname=" . DB . ";charset=" . CHARSET;
-    $pdo = new PDO($dsn, USER, PASSWORD);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
-}
 ?>
 
