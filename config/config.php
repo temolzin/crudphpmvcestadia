8c22c094f15fcc008dfc6208cf5a8819ebec99fa
SJAmanda <157259584+SJAmanda@users.noreply.github.com>
2025-09-09 15:38:28 -0600

    Update config.php
    I deleted this document since it was not supposed to be touched.

--- config/config.php
+++ config/config.php
@@ -1,10 +1,1 @@
-<?php
-define('URL', 'http://localhost/crudphpmvcestadia/');
-define('HOST', 'localhost');
-define('DB', 'estructuramvc');
-define('USER', 'root');
-define('PASSWORD', '');
-define('CHARSET', 'utf8mb4');
-
-?>
 
